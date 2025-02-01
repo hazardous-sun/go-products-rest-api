eefdd46a0604a130ac109383b339a86655df089e
solaire <contact@unharmfulhazard.com>
2025-02-01 16:39:14 -0300

    added an endpoint for creating new products

--- usecase/product_usecase.go
+++ usecase/product_usecase.go
@@ -18,3 +18,17 @@ func NewProductUsecase(repo repository.ProductRepository) ProductUsecase {
 func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
 	return pu.repository.GetProducts()
 }
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+	id, err := pu.repository.CreateProduct(product)
+
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	return model.Product{
+		ID:    id,
+		Name:  product.Name,
+		Price: product.Price,
+	}, nil
+}
