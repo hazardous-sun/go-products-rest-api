bf83111dd2adf885410c1f1c4ea0e9175a555d7b
solaire <contact@unharmfulhazard.com>
2025-02-02 14:04:16 -0300

    Altered the type of Config.Port from string to int

--- db/config.go
+++ db/config.go
@@ -3,19 +3,20 @@ package db
 import (
 	"log"
 	"os"
+	"strconv"
 )
 
 type Config struct {
 	Host     string
-	Port     string
+	Port     int
 	User     string
 	Password string
 	DBName   string
 }
 
-func LoadConfig() *Config {
-	defaultDBHost := "go_db"
-	defaultDBPort := "5432"
+func LoadConfig() Config {
+	defaultDBHost := "localhost"
+	defaultDBPort := 5432
 	defaultDBUser := "postgres"
 	defaultDBPassword := "1234"
 	defaultDBName := "postgres"
@@ -23,43 +24,52 @@ func LoadConfig() *Config {
 	Host := os.Getenv("DB_HOST")
 
 	if Host == "" {
-		log.Println("DB_HOST environment variable not set, setting to default: " + defaultDBHost)
+		log.Println("DB_HOST environment variable not set, cascading to default: " + defaultDBHost)
 		Host = defaultDBHost
 	}
 
-	Port := os.Getenv("DB_PORT")
+	var Port int
+	inputPort := os.Getenv("DB_PORT")
 
-	if Port == "" {
-		log.Println("DB_PORT environment variable not set, setting to default: " + defaultDBPort)
+	if inputPort == "" {
+		log.Println("DB_PORT environment variable not set, cascading to default:", defaultDBPort)
 		Port = defaultDBPort
+	} else {
+		var err error
+		Port, err = strconv.Atoi(inputPort)
+		if err != nil {
+			log.Println("DB_PORT environment variable not set, cascading to default: " + inputPort)
+			Port = defaultDBPort
+		}
 	}
 
 	User := os.Getenv("DB_USER")
 
 	if User == "" {
-		log.Println("DB_USER environment variable not set, setting to default: " + defaultDBUser)
+		log.Println("DB_USER environment variable not set, cascading to default: " + defaultDBUser)
 		User = defaultDBUser
 	}
 
 	Password := os.Getenv("DB_PASSWORD")
 
 	if Password == "" {
-		log.Println("DB_PASSWORD environment variable not set, setting to default: " + defaultDBPassword)
+		log.Println("DB_PASSWORD environment variable not set, cascading to default: " + defaultDBPassword)
 		Password = defaultDBPassword
 	}
 
 	DBName := os.Getenv("DB_NAME")
 
 	if DBName == "" {
-		log.Println("DB_NAME environment variable not set, setting to default: " + defaultDBName)
+		log.Println("DB_NAME environment variable not set, cascading to default: " + defaultDBName)
 		DBName = defaultDBName
 	}
 
-	return &Config{
+	config := Config{
 		Host:     Host,
 		Port:     Port,
 		User:     User,
 		Password: Password,
 		DBName:   DBName,
 	}
+	return config
 }
